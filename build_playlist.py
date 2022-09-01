dd071d34fd150f563078fb729a747f737465f892
Stu W <stubillwhite@gmail.com>
2022-09-01 22:21:00 +0100

    Working playlist

--- build_playlist.py
+++ build_playlist.py
@@ -1,11 +1,14 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 from itertools import *
 from random import shuffle
 import os
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+scope = 'playlist-modify-public'
+
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, redirect_uri='http://localhost:8080'))
 
 
 class Track:
@@ -15,6 +18,7 @@ class Track:
         track = spotify.track(url)
 
         this.submitter = submitter
+        this.id = track['id']
         this.url = track['external_urls']['spotify']
         this.track_name = track['name']
         this.artists = ' / '.join([artist['name'] for artist in track['artists']])
@@ -47,8 +51,18 @@ shuffle(tracks)
 for t in tracks:
     
     print(t.submitter)
+    print(t.id)
     print(t.url)
     print(t.track_name)
     print(t.artists)
 
     print('------')
+
+user_id = spotify.current_user()['id']
+
+playlist = spotify.user_playlist_create(user=user_id, name='Test playlist')
+playlist_id = playlist['id']
+
+track_ids = [t.id for t in tracks]
+
+spotify.user_playlist_add_tracks(user=user_id, playlist_id=playlist_id, tracks=track_ids)
